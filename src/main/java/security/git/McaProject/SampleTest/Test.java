75e7c5fbe42934ef685278cf09b2873bdb8f39b1
Himanshu0k <hkhimanshu1406@gmail.com>
2026-04-17 14:17:41 +0530

    Testing Sensitive Data DB

--- src/main/java/security/git/McaProject/SampleTest/Test.java
+++ src/main/java/security/git/McaProject/SampleTest/Test.java
@@ -4,6 +4,7 @@ public class Test {
     private static final String API_KEY = "sk-1234567890-SECRET";
 
     public void authenticate() {
+        System.out.println("check");
         System.out.println("Using API Key: " + API_KEY);
     }
 }
