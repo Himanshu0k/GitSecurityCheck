8e8fe6b1479a87c891361c7474fe8567225d016a
Himanshu0k <hkhimanshu1406@gmail.com>
2026-04-17 13:47:40 +0530

    Testing Sensitive Data DB

--- src/main/java/security/git/McaProject/SampleTest/Test.java
+++ src/main/java/security/git/McaProject/SampleTest/Test.java
@@ -1,22 +1,9 @@
 package security.git.McaProject.SampleTest;
 
 public class Test {
-    // Bad: Using a global static variable to hold a simple result
-    public static int sum;
+    private static final String API_KEY = "sk-1234567890-SECRET";
 
-    String user = "postgresql";//localhost:5432/GitLogChecks
-    String username="postgres";
-    String password="Himanshu@142002";
-//    spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect
-//    spring.datasource.driver-class-name=org.postgresql.Driver
-
-    public void add() {
-
-        System.out.println("We are inside out Test File ....");
-        // Bad: Hardcoding values instead of using parameters
-        // This method can ONLY ever add 5 and 10.
-        sum = 5 + 10;
-        System.out.println("The result is: " + sum);
-        System.out.println(user + " " + username + " " + password);
+    public void authenticate() {
+        System.out.println("Using API Key: " + API_KEY);
     }
 }
