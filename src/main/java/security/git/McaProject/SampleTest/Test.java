016bacda15861920214679dd99005448404ff9fb
Himanshu0k <hkhimanshu1406@gmail.com>
2026-04-17 16:01:48 +0530

    Testing infinite loop

--- src/main/java/security/git/McaProject/SampleTest/Test.java
+++ src/main/java/security/git/McaProject/SampleTest/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public void run() {
         String password = "1234";
         while (true) {
-            System.out.println("Lets check if it works or not");
+//            System.out.println("Lets check if it works or not");
             System.out.println("Running...");
         }
     }
