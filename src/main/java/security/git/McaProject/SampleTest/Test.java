4f211e46bd825aa0647439b0fa513a17065da2c3
Himanshu0k <hkhimanshu1406@gmail.com>
2026-04-17 15:09:08 +0530

    Testing infinite loop

--- src/main/java/security/git/McaProject/SampleTest/Test.java
+++ src/main/java/security/git/McaProject/SampleTest/Test.java
@@ -1,10 +1,9 @@
 package security.git.McaProject.SampleTest;
 
 public class Test {
-    private static final String API_KEY = "sk-1234567890-SECRET";
-
-    public void authenticate() {
-        System.out.println("Lets check it once again ....");
-        System.out.println("Using API Key: " + API_KEY);
+    public void run() {
+        while (true) {
+            System.out.println("Running...");
+        }
     }
 }
