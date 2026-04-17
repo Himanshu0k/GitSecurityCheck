5ee6a32a3f8f5a5e327a999fbc30ea136b4d7123
Himanshu0k <hkhimanshu1406@gmail.com>
2026-04-17 17:11:46 +0530

    Testing infinite loop logic

--- src/main/java/security/git/McaProject/SampleTest/Test.java
+++ src/main/java/security/git/McaProject/SampleTest/Test.java
@@ -2,8 +2,6 @@ package security.git.McaProject.SampleTest;
 
 public class Test {
     public void run() {
-//        String password = "1234";
-//        System.out.println("lets try again");
         int i = 10;
         while(i > 0) {
             System.out.println("hello");
