74891f6e30d15e22178b07276e5d64953ad9cdcf
Himanshu0k <hkhimanshu1406@gmail.com>
2026-04-17 13:34:01 +0530

    Testing Sensitive Data

--- src/main/java/security/git/McaProject/SampleTest/Test.java
+++ src/main/java/security/git/McaProject/SampleTest/Test.java
@@ -4,6 +4,12 @@ public class Test {
     // Bad: Using a global static variable to hold a simple result
     public static int sum;
 
+    String user = "postgresql";//localhost:5432/GitLogChecks
+    String username="postgres";
+    String password="Himanshu@142002";
+//    spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect
+//    spring.datasource.driver-class-name=org.postgresql.Driver
+
     public void add() {
 
         System.out.println("We are inside out Test File ....");
