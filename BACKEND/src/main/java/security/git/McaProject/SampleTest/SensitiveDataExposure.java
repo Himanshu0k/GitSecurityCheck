f0c7b3166802f0bfe79fac8a19f809b5e9522806
Himanshu0k <hkhimanshu1406@gmail.com>
2026-05-18 04:17:11 +0530

    Checking Data Leaks

--- BACKEND/src/main/java/security/git/McaProject/SampleTest/SensitiveDataExposure.java
+++ BACKEND/src/main/java/security/git/McaProject/SampleTest/SensitiveDataExposure.java
@@ -7,7 +7,7 @@ public class SensitiveDataExposure {
         // AWS Secret Key: AKIAIOSFODNN7EXAMPLE
         // Database Password: admin@123
         // Internal API Token: prod-token-987654
-
+        int a = 10;
         String apiKey = "sk_live_51H8xExampleSecretKey";
         String databasePassword = "SuperSecurePassword123";
         String jwtSecret = "my-jwt-secret-key";
