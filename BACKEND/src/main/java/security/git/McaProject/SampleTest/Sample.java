1578f54b4958b4241c1339eeb2d8ab7271350384
Himanshu0k <hkhimanshu1406@gmail.com>
2026-05-18 00:36:26 +0530

    new changes

--- BACKEND/src/main/java/security/git/McaProject/SampleTest/Sample.java
+++ BACKEND/src/main/java/security/git/McaProject/SampleTest/Sample.java
@@ -3,7 +3,7 @@ package security.git.McaProject.SampleTest;
 public class Sample {
     public void run() {
         while(true) {
-            System.out.println("1");
+            System.out.println("hello world");
         }
     }
 }
