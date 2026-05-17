ad33254b8a76064dbab23902312a79044de4fad1
Himanshu0k <hkhimanshu1406@gmail.com>
2026-05-18 00:01:05 +0530

    new changes

--- BACKEND/src/main/java/security/git/McaProject/SampleTest/CountdownRunner.java
+++ BACKEND/src/main/java/security/git/McaProject/SampleTest/CountdownRunner.java
@@ -8,7 +8,7 @@ public class CountdownRunner {
     private static final Logger logger = LoggerFactory.getLogger(CountdownRunner.class);
 
     public void run() {
-//        System.out.println("just checking ....");
+        System.out.println("just checking ....");
         System.out.println("hello hello");
         int countdown = 10;
         while (countdown > 0) {
