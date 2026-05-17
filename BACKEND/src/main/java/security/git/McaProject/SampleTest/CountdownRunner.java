e94d5913e9a4ca84598266b598a786a1105ce8ac
Himanshu0k <hkhimanshu1406@gmail.com>
2026-05-18 04:00:51 +0530

    Countdown logic Check

--- BACKEND/src/main/java/security/git/McaProject/SampleTest/CountdownRunner.java
+++ BACKEND/src/main/java/security/git/McaProject/SampleTest/CountdownRunner.java
@@ -1,19 +1,21 @@
-//package security.git.McaProject.SampleTest;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//public class CountdownRunner {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(CountdownRunner.class);
-//
-//    public void run() {
-//        System.out.println("just checking ....");
-//        System.out.println("hello hello");
-//        int countdown = 10;
-//        while (countdown > 0) {
-//            logger.info("Countdown: {}", countdown);
-//            countdown--;
-//        }
-//    }
-//}
+package security.git.McaProject.SampleTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CountdownRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(CountdownRunner.class);
+
+    public void runCountdown() {
+
+        int countdown = 5;
+
+        while (countdown > 0) {
+            logger.info("Countdown: {}", countdown);
+            countdown--;
+        }
+
+        logger.info("Countdown completed successfully.");
+    }
+}
